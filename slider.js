cb3221dfefb51f512e6535a3cd254c719b7eb422
Josimo Lopes <joe.lopes@outlook.com>
2024-08-22 11:00:35 -0400

    update distances events

--- slider.js
+++ slider.js
@@ -2,20 +2,32 @@ export default class Slider {
   constructor(wrapper, slider) {
     this.wrapper = document.querySelector(wrapper);
     this.slider = document.querySelector(slider);
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slider.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePosition(pageX) {
+    this.dist.movement = (this.dist.startX - pageX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
   }
 
   onStart(ev) {
     ev.preventDefault();
+    this.dist.startX = ev.pageX;
     this.wrapper.addEventListener("mousemove", this.onMove);
-
-    console.log(this);
   }
 
-  onMove() {
-    console.log("moving");
+  onMove(ev) {
+    const finalPosition = this.updatePosition(ev.pageX);
+    this.moveSlide(finalPosition);
   }
 
-  onEnd() {
+  onEnd(ev) {
+    this.dist.finalPosition = this.dist.movePosition;
     this.wrapper.removeEventListener("mousemove", this.onMove);
   }
 
