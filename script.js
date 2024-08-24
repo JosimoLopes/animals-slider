49b9048a09734f15661aa8197fa4ce58f81fc998
Josimo Lopes <joe.lopes@outlook.com>
2024-08-24 12:29:52 -0400

    add custom control

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ import { SliderNav } from "./slider.js";
 const slider = new SliderNav(".wrapper", ".slider");
 slider.init();
 slider.addArrow(".prev", ".next");
-slider.addControl();
+slider.addControl(".custom__control");
